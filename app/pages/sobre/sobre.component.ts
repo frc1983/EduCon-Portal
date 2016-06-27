f43ef7fbcc684f25f8b756bc2e49fef052127960
lucasmedi <lucas.cunha2104@gmail.com>
2016-06-27 19:35:08 -0300

    Página de informações sobre o trabalho

---
+++ app/pages/sobre/sobre.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from 'angular2/core';
+import { Router } from 'angular2/router';
+
+@Component({
+  selector: 'sobre',
+  templateUrl: 'app/pages/sobre/sobre.component.html',
+  styleUrls: ['app/pages/sobre/sobre.component.css']
+})
+
+export class SobreComponent implements OnInit {
+    
+}
